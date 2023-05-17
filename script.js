51121312b0006ee90e991ea7e8575725c8db474a
wesley berto <wesleyberto4@gmail.com>
2023-05-17 18:45:03 -0300

    horario do dia

---
+++ script.js
@@ -0,0 +1,25 @@
+function carregar(){
+    var msg = document.getElementById("msg");
+    var img = document.getElementById("imagem")
+    var horario = new Date();
+    var hora = 1;
+
+    if(hora >= 0 && hora < 12){
+        //bom dia
+        img.src = "img/manha.png"
+        msg.innerText=`Bom dia! Agora são ${hora} horas`
+        img.style="width: 250px"
+        document.body.style.background="#AABDC4"
+    }else if(hora >= 12 && hora < 18){
+        //boa tarde
+        img.src = "img/tarde.png"
+        msg.innerText=`Boa tarde! Agora são ${hora} horas `
+        img.style ="width: 60%"
+        document.body.style.background="#DBDEDB"
+    }else {
+        img.src ="img/noite.png"
+        msg.innerHTML=`Boa noite! Agora são ${hora} horas`
+        img.style ="width: 280px"
+        document.body.style.background ="#594A91"
+    }
+}
